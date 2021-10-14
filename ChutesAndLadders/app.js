0c581e9f2f0dc85fd6be2ef10ca67bfa6e81b60e
pmarchand122 <pmarchan@iu.edu>
2021-10-14 14:25:56 -0400

    finished project 1

--- ChutesAndLadders/app.js
+++ ChutesAndLadders/app.js
@@ -1,27 +1,33 @@
 class Board {
   gameZone = document.getElementById("board");
   numCells = 99;
-  numLadders = 5;
-  numChutes = 5;
-  chance = 0.4;
   dice = new Dice();
   result = new resultDisplay();
 
   constructor() {
+    //Cell display properties
     let xPos = 0;
     let row = 0;
     let yPos = 0;
+
+    //Array of cells
     let cellList = [];
+
+    //Initial position current cell
     var currentPosition = 98;
+
+    //Number of times the "Roll Dice" button is clicked
     let rollNum = 0;
 
+    //Create the board
     for (var i = 0; i < this.numCells; i++) {
       let newCell = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "rect"
       );
-      newCell.classList.add("cell");
-      newCell.classList.add("normal");
+
+      //Cell's style and positioning on the board
+      newCell.classList.add("cell", "normal");
       newCell.setAttribute("height", 70 + "px");
       newCell.setAttribute("width", 75 + "px");
       newCell.setAttribute("x", xPos);
@@ -29,10 +35,14 @@ class Board {
       newCell.setAttribute("id", "normal");
 
       console.log(newCell);
+
+      //Add each new cell to an array so that they can then be individually targeted
       cellList.push(newCell);
 
+      //Add each new cell to the board
       this.gameZone.appendChild(newCell);
 
+      //If there are already 8 cells in a row, reset the xPos to 0 and make the yPos go down to start a new row and reset the row to zero. If the row is not at 8 yet, add 1 to row  and move each cell 75px on the x-axis.
       if (row >= 8) {
         yPos += 70;
         xPos = 0;
@@ -43,14 +53,22 @@ class Board {
       }
     }
 
-    //Settings for chutes
-    cellList[10].setAttribute("id", "chute");
-    cellList[59].setAttribute("id", "chute");
-    cellList[89].setAttribute("id", "chute");
+    //Animation that displays each cell on the screen one by one
+    for (var i = 0; i < cellList.length; i++) {
+      TweenLite.from(cellList[i], {
+        duration: 0.6,
+        alpha: 0,
+        delay: i * 0.01,
+      });
+    }
 
-    //settings for ladders
-    cellList[82].setAttribute("id", "ladder");
-    cellList[39].setAttribute("id", "ladder");
+    //Randomly assign where the chutes and ladders will be assigned on the board
+    for (var i = 0; i < 20; i++) {
+      cellList[Math.floor(Math.random() * 89)].setAttribute("id", "chute");
+      cellList[Math.floor(Math.random() * 95)].setAttribute("id", "ladder");
+    }
+
+    //If a cell's id is "chute" or "ladder", add their respective classes.
     for (var i = 0; i < cellList.length; i++) {
       if (cellList[i].id == "chute") {
         cellList[i].classList.add("chute");
@@ -60,44 +78,61 @@ class Board {
       }
     }
 
-    //settings for current position
-
+    //Dice roll listener
     this.dice.btn.addEventListener("click", () => {
       if (currentPosition <= 0) {
-        this.gameZone.style.display = "none";
-        this.result.resultsContainer.style.display = "flex";
+        //If the current position is at 0, trigger animations that make the board, and di button and display disappear
+        TweenLite.to(this.gameZone, { duration: 0.6, alpha: 0 });
+        TweenLite.to(this.dice.btn, { duration: 0.6, alpha: 0 });
+        TweenLite.to(this.dice.rollDisp, { duration: 0.6, alpha: 0 });
+
+        //Set the results container div's display to block to display it on the screen
+        this.result.resultsContainer.style.display = "block";
+
+        //use the displayFinalResult function from the resultDisplay class to display how many rolls it took for the game to be completed
         this.result.displayFinalResult(rollNum);
       } else {
+        //If the current position is NOT at 0, at 1 to the amount of times the di was rolled
         rollNum++;
-        console.log(rollNum);
-        cellList[currentPosition].classList.remove("current");
-        let value = Math.floor(Math.random() * 6);
-        console.log(value);
-        currentPosition = currentPosition - value;
-        cellList[currentPosition].classList.add("current");
-        this.dice.displayDiceResult(value);
-        console.log(currentPosition);
 
+        //Remove the CSS class from the current cell
+        cellList[currentPosition].classList.remove("current");
+
+        //Asign a random value from 1 to 6
+        let value = Math.floor(Math.random() * 6) + 1;
+
+        //Update the current position to subtract the amount from the random value which moves the current position up the board
+        currentPosition = currentPosition - value;
+
+        //Re assign the CSS class to the new current position
+        cellList[currentPosition].classList.add("current");
+
+        //use the displayDiceResult function from the Dice class to display the random value that was applied to the currentPosition
+        this.dice.displayDiceResult(value);
+
+        //If the ID of the current position is equal to "chute" or "ladder", remove the current class, subtract or add a random number between 1 and 10 to the current position, add the current class to the new position
         if (cellList[currentPosition].id == "chute") {
           cellList[currentPosition].classList.remove("current");
-          currentPosition = currentPosition + 10;
+          currentPosition =
+            currentPosition + Math.floor(Math.random() * 10) + 1;
           cellList[currentPosition].classList.add("current");
         }
 
         if (cellList[currentPosition].id == "ladder") {
           cellList[currentPosition].classList.remove("current");
-          currentPosition = currentPosition - 10;
+          currentPosition =
+            currentPosition - Math.floor(Math.random() * 10) + 1;
           cellList[currentPosition].classList.add("current");
         }
       }
     });
 
+    //Add the current class for the initial currentPosition which is the start of the game
     cellList[currentPosition].classList.add("current");
-
-    console.log(currentPosition);
   }
 }
 
+//Di class which displays the random value from the "Roll Dice" button
 class Dice {
   rollDisp = document.getElementById("diceResult");
   btn = document.getElementById("roll");
@@ -107,6 +142,7 @@ class Dice {
   }
 }
 
+//Result Dispaly which displays the amount of rolls it took for the game to be completed. Results are displayed at the end of the game
 class resultDisplay {
   resultsContainer = document.getElementById("resultsContainer");
   results = document.getElementById("results");
